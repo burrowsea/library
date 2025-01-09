5ce57fc102a1c8f42527c81d6e0f36136650c7b8
burrows.ea <burrows.ea@oundleschool.org.uk>
2025-01-09 10:12:12 +0000

    a

--- install.php
+++ install.php
@@ -5,8 +5,8 @@ CREATE TABLE tblusers
 (userid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 surname VARCHAR(20) NOT NULL,
 forename VARCHAR(20) NOT NULL,
-password VARCHAR(20) NOT NULL
-dob VARCHAR(10) NOT NULL
+password VARCHAR(20) NOT NULL,
+dob VARCHAR(10) NOT NULL,
 email VARCHAR(30) NOT NULL)"
 );
 $stmt->execute();
@@ -16,6 +16,8 @@ echo("tblusers created");
 $stmt = $conn->prepare("DROP TABLE IF EXISTS tblbooks;
 CREATE TABLE tblbooks
 (bookid INT(14) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(100) NOT NULL,
+author VARCHAR(50) NOT NULL,
 )"
 );
 $stmt->execute();
