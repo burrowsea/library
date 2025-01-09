4d6df38eec2384119744daf7c7085d015a00293e
burrows.ea <burrows.ea@oundleschool.org.uk>
2025-01-09 10:01:09 +0000

    tblusers created

--- install.php
+++ install.php
@@ -1,2 +1,13 @@
 <?php
 include_once("connection.php");
+$stmt = $conn->prepare("DROP TABLE IF EXISTS tblusers;
+CREATE TABLE tblusers 
+(userid INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+surname VARCHAR(20) NOT NULL,
+forename VARCHAR(20) NOT NULL,
+password VARCHAR(20) NOT NULL
+dob VARCHAR(10) NOT NULL)"
+);
+$stmt->execute();
+$stmt->closeCursor();
+echo("tblusers created");
