2d3000d814d83da46af775c5d9b436c07583ab03
jazzellee <lee.y@oundleschool.org.uk>
2025-01-14 10:03:40 +0000

    jyug

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ $username="root";
 $password="";
 $dbname="library";
 
-$conn=new PDO("mysql:host=$servername; dbname=$dbname" ,$username,$password);
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 #echo("connected successfully");
 ?>
--- users.php
+++ users.php
@@ -9,18 +9,17 @@
     <form action="addusers.php" method="POST">
     First name:<input type="text" name="forename"><br>
     Last name:<input type="text" name="surname"><br>
-    Password:<input type="password" name="passwd"><br>
-    House:<input type="text" name="house"><br>
-    Year:<input type="text" name="year"><br>
+    Password:<input type="password" name="password"><br>
+    Date of Birth (xx/xx/xx):<input type="text" name="dob"><br>
+    Email Address:<input type="text" name="email"><br>
     <!--Creates a drop down list-->
-    Gender:<select name="gender">
+    Gender:<select name="dob">
             <option value="M">Male</option>
             <option value="F">Female</option>
         </select>
     <br>
     <!--Next 3 lines create a radio button which we can use to select the user role-->
-    <input type="radio" name="role" value="Pupil" checked> Pupil<br>
-    <input type="radio" name="role" value="Teacher"> Teacher<br>
+    <input type="radio" name="role" value="User" checked> Pupil<br>
     <input type="radio" name="role" value="Admin"> Admin<br>
     <input type="submit" value="Add User">
     </form>
