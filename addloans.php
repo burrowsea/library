dbb1135d0ceeff8a25adaaf3fef363e5b946183c
jazzellee <lee.y@oundleschool.org.uk>
2025-01-21 10:29:29 +0000

    idk

--- addloans.php
+++ addloans.php
@@ -5,8 +5,10 @@ if (
     isset($_POST["userid"], $_POST["bookid"], $_POST["isbn"], $_POST["borrowdate"], $_POST["status"])
 ) {
     array_map("htmlspecialchars", $_POST);
-    $borrow_date = date(strtotime($_POST["borrowdate"]));//date you get from database
-    $duedate = date_add($borrow_date, date_interval_create_from_date_string("14 days"));
+    $date = date_create($_POST["borrowdate"]);
+    $duedate = date_add($date, date_interval_create_from_date_string("14 days"));
+    $duedate_unix = strtotime($duedate);
+    $duedate_string = date("Y-m-d", $duedate_unix)
 
     switch ($_POST["status"]) {
         case "On loan":
@@ -19,7 +21,7 @@ if (
             $status = null;
     }
 
-    if ($status !== null) {
+    if ($status == 0 or $status == 1) {
         //try {
             $stmt = $conn->prepare("INSERT INTO tblloans (userid, bookid, isbn, borrowdate, duedate, status)
                 VALUES (:userid, :bookid, :isbn, :borrowdate, :duedate, :status)");
@@ -27,7 +29,7 @@ if (
             $stmt->bindParam(':bookid', $_POST["bookid"]);
 			$stmt->bindParam(':isbn', $_POST["isbn"]);
             $stmt->bindParam(':borrowdate', $borrow_date);
-            $stmt->bindParam(':duedate', $duedate);
+            $stmt->bindParam(':duedate', $duedate_string);
             $stmt->bindParam(':status', $status);
 
             $stmt->execute();
@@ -40,7 +42,8 @@ if (
         //    error_log("Database error: " . $e->getMessage());
          //   echo "An error occurred. Please try again later.";
         //}
-    } else {
+    }
+     else {
         echo "Please select loan status.";
     }
 } else {
