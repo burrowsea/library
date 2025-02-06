76f3c8e015f5d2f0aee754c430fc1b3a166cad33
jazzellee <lee.y@oundleschool.org.uk>
2025-02-06 10:13:04 +0000

    同意

--- admin.php
+++ admin.php
@@ -27,6 +27,5 @@ if (!isset($_SESSION['admin_logged_in'])) {
         <div class="mt-6 text-center">
             <a href="logout.php" class="bg-red-500 text-white px-4 py-2 rounded">Logout</a>
         </div>
-    </div>
-</body>
+    </div></body>
 </html>
